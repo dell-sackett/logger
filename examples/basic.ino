d38de5b6b268188c05985235747c4ee9f72cebe2
Dell Sackett <dellray.sackett@gmail.com>
2025-12-13 22:15:34 -0500

    Fixed up the example

--- examples/basic.ino
+++ examples/basic.ino
@@ -3,19 +3,32 @@
 int counter = 0;
 
 void setup() {
+
+    // Start the task
     xTaskCreate(tskLogger,
             "Logging Task",
             LOGGER_MEM,
             NULL,
             1,
             NULL);
+    
+    // Wait 2 milliseconds for the logger to be ready
+    vTaskDelay(2 / portTICK_PERIOD_MS);
+
 }
 
 void loop() {
 
-    logWrite(INFO, "This is log message " + counter + "!");
+    // Put together a string
+    std::string message = "This is a log message ";
+    message += std::to_string(counter);
+    message += "!";
+
+    // Send the string to the logger
+    logWrite(INFO, message);
     counter += 1;
     
+    // Wait 2 seconds
     vTaskDelay(2000 / portTICK_PERIOD_MS);
 
 }
